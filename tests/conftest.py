ca8137ca643accd0abe1c0c0a760c156f027eee7
Krasilnikov_Ivan <ikra-nn@yandex.ru>
2025-05-09 20:05:05 +0300

    исправлены ошибки

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,7 +14,7 @@ def pytest_configure(config):
 @pytest.fixture(scope="session")
 def browser(playwright: Playwright):
     logger.info("Запуск браузера Chromium в режиме без headless")
-    browser = playwright.chromium.launch(headless=False)
+    browser = playwright.chromium.launch(headless=True)
     yield browser
     logger.info("Закрытие браузера")
     browser.close()
