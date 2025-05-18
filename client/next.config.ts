71cfbea3215293e57d233b7caefeb914c2df40c4
Krasilnikov_Ivan <ikra-nn@yandex.ru>
2025-05-18 18:44:47 +0300

    исправлено

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,17 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  /* config options here */
+};
+module.exports = {
   images: {
-    domains: ["avatar.iran.liara.run"],
-  },
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "http://localhost:3001/:path*",
-      },
-    ];
+    domains: ['avatar.iran.liara.run'],
   },
 };
-
 export default nextConfig;
--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import { localSignup, localLogin, logout } from '../controllers/authController';
 
+
 const router = express.Router();
 
-router.post('/api/auth/signup', localSignup);
-router.post('/api/auth/login', localLogin);
-router.post('/api/auth/logout', logout);
+router.post('/signup', localSignup);
+router.post('/login', localLogin);
+router.post('/logout', logout );
 
 export default router;
